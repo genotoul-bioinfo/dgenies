b2af07eda233ee8483c8b377670d84d1ce91b846
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2017-10-06 17:05:40 +0200

    Fix test php files

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-	<script src="imapdot/js/jquery-3.2.1.min.js" type="text/JavaScript"></script>
-	<script src="imapdot/js/jquery-ui.min.js" type="text/JavaScript"></script>
-	<script src="imapdot/js/sprintf.min.js" type="text/JavaScript"></script>
-	<script src="imapdot/js/d3.min.js" type="text/JavaScript"></script>
-	<script src="imapdot/js/draw_graph.js" type="text/JavaScript"></script>
-	<link rel="stylesheet" href="imapdot/css/jquery-ui.min.css" type="text/css">
-	<link rel="stylesheet" href="imapdot/css/style.css" type="text/css">
+	<script src="igenocomp/js/jquery-3.2.1.min.js" type="text/JavaScript"></script>
+	<script src="igenocomp/js/jquery-ui.min.js" type="text/JavaScript"></script>
+	<script src="igenocomp/js/sprintf.min.js" type="text/JavaScript"></script>
+	<script src="igenocomp/js/d3.min.js" type="text/JavaScript"></script>
+	<script src="igenocomp/js/draw_graph.js" type="text/JavaScript"></script>
+	<link rel="stylesheet" href="igenocomp/css/jquery-ui.min.css" type="text/css">
+	<link rel="stylesheet" href="igenocomp/css/style.css" type="text/css">
 </head>
 <body onload="d3.boxplot.init();">
 <div id="subdraw">
