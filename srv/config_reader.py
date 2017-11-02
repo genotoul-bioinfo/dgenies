bef01341607bf6eef18e32d587da2953d95bfbc9
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2017-11-02 17:10:16 +0100

    Make applications.properties genric and set it as example, Fixed #30

--- srv/config_reader.py
+++ srv/config_reader.py
@@ -12,18 +12,27 @@ class AppConfigReader(object):
     def __init__(self):
         """
         """
+        config_file = os.path.join(os.path.dirname(inspect.getfile(self.__class__)), self.CONFIG_FILE_PATH)
+        if not os.path.exists(config_file):
+            raise FileNotFoundError("ERROR: application.properties not found. Please copy the example file and check "
+                                    "properties are correct for you!")
         self.reader = RawConfigParser()
-        self.reader.read(os.path.join(os.path.dirname(inspect.getfile(self.__class__)), self.CONFIG_FILE_PATH))
+        self.reader.read(config_file)
+
+    @staticmethod
+    def replace_vars(path):
+        return path.replace("###USER###", os.path.expanduser("~"))\
+            .replace("###PROGRAM###", os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
     def get_upload_folder(self):
         try:
-            return self.reader.get("global", "upload_folder")
+            return self.replace_vars(self.reader.get("global", "upload_folder"))
         except NoOptionError:
             raise Exception("No upload folder found in application.properties (global section)")
 
     def get_app_data(self):
         try:
-            return self.reader.get("global", "data_folder")
+            return self.replace_vars(self.reader.get("global", "data_folder"))
         except NoOptionError:
             raise Exception("No data folder found in application.properties (global section)")
 
@@ -41,12 +50,13 @@ class AppConfigReader(object):
 
     def get_minimap2_exec(self):
         try:
-            return self.reader.get("softwares", "minimap2")
+            entry = self.reader.get("softwares", "minimap2")
+            return entry if entry != "###DEFAULT###" else "minimap2"
         except NoOptionError:
             return "minimap2"
 
     def get_database(self):
         try:
-            return self.reader.get("database", "sqlite_file")
+            return self.replace_vars(self.reader.get("database", "sqlite_file"))
         except NoOptionError:
             return ":memory:"
