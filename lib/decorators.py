37af6051bdc5c1333e33e6486e0e2b0da71132e0
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2017-11-27 18:05:03 +0100

    Add missing file

---
+++ lib/decorators.py
@@ -0,0 +1,9 @@
+class Singleton:
+    def __init__(self, klass):
+        self.klass = klass
+        self.instance = None
+
+    def __call__(self, *args, **kwds):
+        if self.instance is None:
+            self.instance = self.klass(*args, **kwds)
+        return self.instance
