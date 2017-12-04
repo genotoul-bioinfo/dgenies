c3f497436d3055cc65127db9f8bbd2df347898c3
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2017-12-04 10:32:51 +0100

    Improve reload on status page

--- js/dgenies.status.js
+++ js/dgenies.status.js
@@ -5,13 +5,30 @@ dgenies.status = {};
 
 
 dgenies.status.init = function (status) {
-    if (status !== "success" && status !== "done" && status !== "no-match") {
+    if (status !== "success" && status !== "done" && status !== "no-match" && status !== "fail") {
         dgenies.status.autoreload();
     }
 };
 
 dgenies.status.autoreload = function () {
+    let get_p = new URLSearchParams(window.location.search);
+    let refresh = get_p.get("refresh") !== null ? parseInt(get_p.get("refresh")) : 1;
+    let count = get_p.get("count") !== null ? parseInt(get_p.get("count")) : 1;
+    if (refresh < 30) {
+        if (refresh % 5 === 0) {
+            if (count > 3) {
+                refresh += 1;
+                count = 1;
+            }
+            else {
+                count += 1
+            }
+        }
+        else {
+            refresh += 1
+        }
+    }
     setTimeout(function(){
-        location.reload();
-    }, 5000)
-}
+        window.location.replace(`?refresh=${refresh}&count=${count}`);
+    }, refresh * 1000)
+};
