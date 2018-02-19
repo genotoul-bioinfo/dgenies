9a1266ec02731edd481978e3ccda708d27e1344b
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2018-02-19 16:11:27 +0100

    Fix add to result in webserver mode

--- src/dgenies/static/js/dgenies.result.js
+++ src/dgenies/static/js/dgenies.result.js
@@ -8,7 +8,21 @@ dgenies.result.id_res = null;
 
 dgenies.result.init = function(id_res) {
     dgenies.result.id_res = id_res;
+    dgenies.result.add_to_list();
     d3.boxplot.init();
+};
+
+dgenies.result.add_to_list = function () {
+    console.log("pass", dgenies.result.id_res);
+    let cookies = $.cookie("results");
+    cookies = cookies !== undefined ? cookies.split("|") : [];
+    console.log(cookies);
+    if (cookies.indexOf(dgenies.result.id_res) === -1) {
+        console.log("oui");
+        cookies.splice(0, 0, dgenies.result.id_res);
+        dgenies.save_cookies(cookies);
+        dgenies.update_results(cookies);
+    }
 };
 
 dgenies.result.remove_job_from_cookie = function(job) {
