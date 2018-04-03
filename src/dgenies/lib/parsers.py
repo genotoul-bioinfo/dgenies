d6ad6b3af293ce38569c7d17c90358a20565fb63
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2018-04-03 13:00:54 +0200

    remove mashmap from packaged tools

--- src/dgenies/lib/parsers.py
+++ src/dgenies/lib/parsers.py
@@ -6,11 +6,3 @@ Each parser (main function) must have 2 and only 2 arguments:
 - Second argument: finale PAF file
 """
 
-
-def mashmap2paf(in_paf, out_paf):
-    with open(in_paf, "r") as in_p, open(out_paf, "w") as out_p:
-        for line in in_p:
-            parts = line.rstrip().split(" ")
-            parts[9] = str(round(float(parts[9]) / 100.0 * 1000.0))
-            parts.append("1000")
-            out_p.write("\t".join(parts) + "\n")
