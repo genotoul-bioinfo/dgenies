67884f3fc6a081217f629603537603eed5118af4
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2018-05-15 13:01:20 +0200

    Fix compatibility to pip 10 + change version to 1.1.1

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import os
 import sys
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 from pathlib import Path
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
+with open('requirements.txt') as f:
+    install_reqs = f.read().strip().split('\n')
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
-version = '1.1.0'
+version = '1.1.1'
 
 if os.name == "posix":
 
@@ -25,7 +24,7 @@ if os.name == "posix":
         package_dir={'dgenies': 'src/dgenies'},
         include_package_data=True,
         zip_safe=False,
-        install_requires=reqs,
+        install_requires=install_reqs,
         data_files=[(config_dir, ['application.properties']),
                     (config_dir, ['tools.yaml']),
                     (wsgi_dir, ['dgenies.wsgi'])],
