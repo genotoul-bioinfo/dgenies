d51d5daa9147a6cde9c259d8616a08285cc9bd6f
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2018-06-22 15:57:47 +0200

    Fix sphinx doc for readthedoc compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+import build_js_doc_rst
+build_js_doc_rst.build_rst_files()
+
 sys.path.insert(0, os.path.join(os.path.abspath('..'), "src"))
 import dgenies
 os.environ['DISABLE_CRONS'] = "True"
