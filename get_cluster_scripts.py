a98d5157c9f4f15be4b6c7c86cdcf71ba786f2b0
Floreal Cabanettes <floreal.cabanettes@inra.fr>
2018-07-10 14:11:23 +0200

    Add mashmap tool

--- get_cluster_scripts.py
+++ get_cluster_scripts.py
@@ -4,7 +4,7 @@ import argparse
 import wget
 import os
 
-VERSION = "1.0.1"
+VERSION = "1.1.1"
 
 parser = argparse.ArgumentParser(description="Split huge contigs")
 parser.add_argument('-d', '--dir', type=str, required=False, help="Folder into store files", default=".")
@@ -15,7 +15,7 @@ for file_dl in [
     "https://raw.githubusercontent.com/genotoul-bioinfo/dgenies/v%s/src/dgenies/bin/all_prepare.py" % VERSION,
     "https://raw.githubusercontent.com/genotoul-bioinfo/dgenies/v%s/src/dgenies/bin/filter_contigs.py" % VERSION,
     "https://raw.githubusercontent.com/genotoul-bioinfo/dgenies/v%s/src/dgenies/bin/index.py" % VERSION,
-    "https://raw.githubusercontent.com/genotoul-bioinfo/dgenies/v%s/src/dgenies/binsplit_fa.py" % VERSION
+    "https://raw.githubusercontent.com/genotoul-bioinfo/dgenies/v%s/src/dgenies/bin/split_fa.py" % VERSION
         ]:
     d_file = os.path.join(args.dir, file_dl.rsplit("/", 1)[1])
     print("Downloading %s..." % d_file)
--- src/dgenies/lib/parsers.py
+++ src/dgenies/lib/parsers.py
@@ -71,3 +71,13 @@ def maf(in_maf, out_paf):
     else:
         maf_f.close()
         return True
+
+
+def mashmap2paf(in_paf, out_paf):
+    with open(in_paf, "r") as in_p, open(out_paf, "w") as out_p:
+        for line in in_p:
+            parts = line.rstrip().split(" ")
+            parts[9] = str(round(float(parts[9]) / 100.0 * 1000.0))
+            parts.append("1000")
+            out_p.write("\t".join(parts) + "\n")
+
