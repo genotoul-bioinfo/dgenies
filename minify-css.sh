27637d4ce5f442f00fa86e3cc7ed214b10086b98
Philippe Bordron <philippe.bordron@inrae.fr>
2022-10-17 16:59:16 +0200

    Update css minification script
    - Change to less
    - use npm scripts

--- minify-css.sh
+++ minify-css.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
-if [ ! $(which index.js) ] ; then
-    echo """
-    ERROR: minifier is not installed!
+set -e
 
-    Install it through npm:
-    npm install --prefix ~ minifier
+# Use `npm install` in dgenies root directory to install build dependencies
 
-    Then, add it to the PATH:
-    bash: export PATH=\$PATH:~/node_modules/minifier
-    fish: set PATH \$PATH ~/node_modules/minifier
-    """
-    exit 1
-fi
-
-cd src/dgenies/static/css
-
-index.js -o dgenies.min.css dgenies.css
-index.js -o dgenies-offline-result.min.css chosen.min.css animate.css jquery-ui.min.css bootstrap.min.css bootstrap-theme.min.css dgenies.min.css
+npm run "minify:css"
