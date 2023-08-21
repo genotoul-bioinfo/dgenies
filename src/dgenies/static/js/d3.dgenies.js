8055fbff53b69e51b4204aeb87f5ad3a4c0fb3d0
Philippe Bordron <philippe.bordron@inrae.fr>
2023-08-21 16:42:48 +0200

    Apply automatically selected color

--- src/dgenies/static/js/d3.dgenies.js
+++ src/dgenies/static/js/d3.dgenies.js
@@ -1283,34 +1283,35 @@ d3.dgenies.create_pickr = function (el, default_color = '#FFFFFF00') {
         lockOpacity: false,
 
         swatches: [
-            'rgba(244, 67, 54, 1)',
-            'rgba(233, 30, 99, 0.95)',
-            'rgba(156, 39, 176, 0.9)',
-            'rgba(103, 58, 183, 0.85)',
-            'rgba(63, 81, 181, 0.8)',
-            'rgba(33, 150, 243, 0.75)',
-            'rgba(3, 169, 244, 0.7)',
-            'rgba(0, 188, 212, 0.7)',
-            'rgba(0, 150, 136, 0.75)',
-            'rgba(76, 175, 80, 0.8)',
-            'rgba(139, 195, 74, 0.85)',
-            'rgba(205, 220, 57, 0.9)',
-            'rgba(255, 235, 59, 0.95)',
-            'rgba(255, 193, 7, 1)'
+            'rgba(244, 67, 54, 0.2)',
+            'rgba(233, 30, 99, 0.2)',
+            'rgba(156, 39, 176, 0.2)',
+            'rgba(103, 58, 183, 0.2)',
+            'rgba(63, 81, 181, 0.2)',
+            'rgba(33, 150, 243, 0.2)',
+            'rgba(3, 169, 244, 0.2)',
+            'rgba(0, 188, 212, 0.2)',
+            'rgba(0, 150, 136, 0.2)',
+            'rgba(76, 175, 80, 0.2)',
+            'rgba(139, 195, 74, 0.2)',
+            'rgba(205, 220, 57, 0.2)',
+            'rgba(255, 235, 59, 0.2)',
+            'rgba(255, 193, 7, 0.2)'
         ],
 
         components: {
-            preview: true,
-            opacity: true,
-            hue: true,
+            palette: false,
+            preview: false,
+            opacity: false,
+            hue: false,
 
             interaction: {
-                hex: true,
-                rgba: true,
-                hsva: true,
-                input: true,
+                hex: false,
+                rgba: false,
+                hsva: false,
+                input: false,
                 clear: true,
-                save: true
+                save: false
             }
         }
     });
@@ -1358,24 +1359,28 @@ d3.dgenies.add_restore_all = function () {
                 `)
         );
     d3.dgenies.x_pickr = d3.dgenies.create_pickr("#xpickr", d3.dgenies.x_background_color)
-        .on('save', (color, instance) => {
-            //console.log('Event: "save"', color, instance);
-            d3.dgenies.x_background_color = color.toRGBA().toString();
+        .on('change', (color, source, instance) => {
+            instance.applyColor()
+        }).on('save', (color, instance) => {
+            d3.dgenies.x_background_color = (color!==null) ? color.toRGBA().toString(): 'rgba(255,255,255,0)';
             d3.dgenies.draw_background(d3.dgenies.x_order, d3.dgenies.y_order);
         });
     $("#rowColor button")
         .addClass("btn btn-sm")
         .attr("id", "xpickr")
         .after(`<label for="xpickr">Row</label>`); 
-    d3.dgenies.y_pickr = d3.dgenies.create_pickr("#ypickr", d3.dgenies.y_background_color);
+    d3.dgenies.y_pickr = d3.dgenies.create_pickr("#ypickr", d3.dgenies.y_background_color)
+        .on('change', (color, source, instance) => {
+            console.log('Event: "change"', color, source, instance);
+            instance.applyColor()
+        }).on('save', (color, instance) => {
+            d3.dgenies.y_background_color = (color!==null) ? color.toRGBA().toString(): 'rgba(255,255,255,0)';
+            d3.dgenies.draw_background(d3.dgenies.x_order, d3.dgenies.y_order);
+        });
     $("#columnColor button")
         .addClass("btn btn-sm")
         .attr("id", "ypickr")
         .after(`<label for="ypickr">Column</label>`);
-    d3.dgenies.y_pickr.on('save', (color, instance) => {
-        d3.dgenies.y_background_color = color.toRGBA().toString();
-        d3.dgenies.draw_background(d3.dgenies.x_order, d3.dgenies.y_order);
-    });
 }
 
 /**
