d8abd070a7dd05f8fdf9b30d791c3b76107f839b
Philippe Bordron <philippe.bordron@inrae.fr>
2023-10-04 15:42:43 +0200

    Add toolips to tracks

--- src/dgenies/static/js/d3.dgenies.mousetip.js
+++ src/dgenies/static/js/d3.dgenies.mousetip.js
@@ -1,7 +1,13 @@
 if (!d3 || !d3.dgenies) {
     throw "d3.dgenies wasn't included!"
 }
+
+
 d3.dgenies.mousetip = {};
+
+d3.dgenies.mousetip.dotplot = {};
+d3.dgenies.mousetip.track_x = {};
+d3.dgenies.mousetip.track_y = {};
 
 /**
  * Get color (black/white) depending on bgColor so it would be clearly seen.
@@ -15,14 +21,14 @@ d3.dgenies.mousetip.getColorByBgColor = function(bgColor) {
 }
 
 /**
- * Mouse tip basis
+ * Mouse tip basis for dotplot
  *
  * @param my_tip
  * @param relative_to
  * @param {int} x
  * @param {int} y
  */
-$.fn.mousetip = function(my_tip, relative_to=null, x=20, y=20) {
+$.fn.mousetip_dotplot = function(my_tip, relative_to=null, x=20, y=20) {
 
     let $this = $(this);
     let tip = relative_to === null ? $(my_tip, this) : $(my_tip, relative_to);
@@ -38,115 +44,219 @@ $.fn.mousetip = function(my_tip, relative_to=null, x=20, y=20) {
         hidden = true;
         tip.hide().removeAttr('style');
 
-    }).mousemove(function(e) {
+    }).mousemove(function (e) {
 
         tip.hide();
 
-            window.setTimeout(() => {
+        window.setTimeout(() => {
 
-                let rect = relative_to === null ? this.getBoundingClientRect() : $(relative_to)[0].getBoundingClientRect();
-                let posX = rect.left + window.scrollX,
-                    posY = rect.top + window.scrollY,
-                    m_x = e.pageX - rect.left - window.scrollX,
-                    m_y = e.pageY - rect.top - window.scrollY;
+            let rect = relative_to === null ? this.getBoundingClientRect() : $(relative_to)[0].getBoundingClientRect();
+            let posX = rect.left + window.scrollX,
+                posY = rect.top + window.scrollY,
+                m_x = e.pageX - rect.left - window.scrollX,
+                m_y = e.pageY - rect.top - window.scrollY;
 
-                let mouseX = m_x + (x);
-                let mouseY = m_y + (y);
+            let mouseX = m_x + (x);
+            let mouseY = m_y + (y);
 
-                let rect_g = $("g.container")[0].getBoundingClientRect();
-                let posX_g = rect_g.left + window.scrollX,
-                    posY_g = rect_g.top + window.scrollY,
-                    width_c = rect_g.width,
-                    height_c = rect_g.height;
-                let x_g = (e.pageX - posX_g) / width_c * d3.dgenies.scale,
-                    y_g = d3.dgenies.scale - ((e.pageY - posY_g) / height_c * d3.dgenies.scale);
+            let rect_g = $("g.container")[0].getBoundingClientRect();
+            let posX_g = rect_g.left + window.scrollX,
+                posY_g = rect_g.top + window.scrollY,
+                width_c = rect_g.width,
+                height_c = rect_g.height;
+            let x_g = (e.pageX - posX_g) / width_c * d3.dgenies.scale,
+                y_g = d3.dgenies.scale - ((e.pageY - posY_g) / height_c * d3.dgenies.scale);
 
-                let match = d3.dgenies.mousetip.get_match(e);
+            let match = d3.dgenies.mousetip.dotplot.get_match(e);
 
-                let x_zone = "unknown";
-                for (let zone in d3.dgenies.x_zones) {
-                    if (d3.dgenies.x_zones[zone][0] < x_g && x_g <= d3.dgenies.x_zones[zone][1]) {
-                        x_zone = d3.dgenies.mousetip.get_label(zone);
-                        break;
-                    }
+            let x_zone = "unknown";
+            for (let zone in d3.dgenies.x_zones) {
+                if (d3.dgenies.x_zones[zone][0] < x_g && x_g <= d3.dgenies.x_zones[zone][1]) {
+                    x_zone = d3.dgenies.mousetip.get_label(zone);
+                    break;
                 }
-                let y_zone = "unknown";
-                for (let zone in d3.dgenies.y_zones) {
-                    if (d3.dgenies.y_zones[zone][0] < y_g && y_g <= d3.dgenies.y_zones[zone][1]) {
-                        y_zone = d3.dgenies.mousetip.get_label(zone);
-                        break;
-                    }
+            }
+            let y_zone = "unknown";
+            for (let zone in d3.dgenies.y_zones) {
+                if (d3.dgenies.y_zones[zone][0] < y_g && y_g <= d3.dgenies.y_zones[zone][1]) {
+                    y_zone = d3.dgenies.mousetip.get_label(zone);
+                    break;
                 }
+            }
 
-                let html = "";
+            let html = "";
 
-                if (match !== null) {
-                    html = `<strong>Query:</strong> ${match.y_zone}<br/>(${match.y_match[0]} - ${match.y_match[1]})<br/>
-                            <strong>Target:</strong> ${match.x_zone}<br/>(${match.x_match[0]} - ${match.x_match[1]})<br/>
-                            <strong>Identity:</strong> ${Math.round(match.idy * 100) / 100}`
+            if (match !== null) {
+                html = `<strong>Query:</strong> ${match.y_zone}<br/>(${match.y_match[0]} - ${match.y_match[1]})<br/>
+                        <strong>Target:</strong> ${match.x_zone}<br/>(${match.x_match[0]} - ${match.x_match[1]})<br/>
+                        <strong>Identity:</strong> ${Math.round(match.idy * 100) / 100}`
+            }
+            else {
+                html = `<table class="drawtooltip">
+                    <tr>
+                        <td class="tt-label">Query:</td><td>${y_zone}</td>
+                    </tr>
+                    <tr>
+                        <td class="tt-label">Target:</td><td>${x_zone}</td>
+                    </tr>
+                    </table>`;
+            }
+
+            tip.html(html);
+
+            let css = { top: mouseY, left: mouseX }
+
+            if (match === null) {
+                css.color = "#000";
+                css.background = "#ededed";
+            }
+            else {
+                let idy_class = "";
+                if (match.idy >= 0.75) {
+                    idy_class = "3";
+                }
+                else if (match.idy >= 0.5) {
+                    idy_class = "2";
+                }
+                else if (match.idy >= 0.25) {
+                    idy_class = "1";
+                }
+                else if (match.idy >= 0) {
+                    idy_class = "0";
                 }
                 else {
-                    html = `<table class="drawtooltip">
-                        <tr>
-                            <td class="tt-label">Query:</td><td>${y_zone}</td>
-                        </tr>
-                        <tr>
-                            <td class="tt-label">Target:</td><td>${x_zone}</td>
-                        </tr>
-                        </table>`;
+                    idy_class = "-1"
                 }
+                css.background = d3.dgenies.color_idy[d3.dgenies.color_idy_theme]["palette"][idy_class];
+                css.color = d3.dgenies.mousetip.getColorByBgColor(css.background)
+            }
 
-                tip.html(html);
+            if (!hidden && ((!e.ctrlKey && !d3.dgenies.zone_selected) || match !== null)) {
+                tip.show().css(css);
+            }
 
-                let css = {top: mouseY, left: mouseX}
-
-                if (match === null) {
-                    css.color = "#000";
-                    css.background = "#ededed";
-                }
-                else {
-                    let idy_class = "";
-                    if (match.idy >= 0.75) {
-                        idy_class = "3";
-                    }
-                    else if (match.idy >= 0.5) {
-                        idy_class = "2";
-                    }
-                    else if (match.idy >= 0.25) {
-                        idy_class = "1";
-                    }
-                    else if (match.idy >= 0) {
-                        idy_class = "0";
-                    }
-                    else {
-                        idy_class = "-1"
-                    }
-                    css.background = d3.dgenies.color_idy[d3.dgenies.color_idy_theme]["palette"][idy_class];
-                    css.color = d3.dgenies.mousetip.getColorByBgColor(css.background)
-                }
-
-                if (!hidden && ((!e.ctrlKey && !d3.dgenies.zone_selected) || match !== null)) {
-                    tip.show().css(css);
-                }
-
-            }, 0);
+        }, 0);
     });
 };
 
+
+$.fn.mousetip_track_x = function (my_tip, relative_to = null, x = 20, y = 20) {
+    let $this = $(this);
+    console.log($this)
+    let tip = relative_to === null ? $(my_tip, this) : $(my_tip, relative_to);
+    let hidden = true;
+
+    $this.hover(function (e) {
+        if (!e.ctrlKey) {
+            tip.show();
+            hidden = false;
+        }
+
+    }, function () {
+        hidden = true;
+        tip.hide().removeAttr('style');
+
+    }).mousemove(function (e) {
+
+        tip.hide();
+
+        window.setTimeout(() => {
+
+            let rect = relative_to === null ? this.getBoundingClientRect() : $(relative_to)[0].getBoundingClientRect();
+            let posX = rect.left + window.scrollX,
+                posY = rect.top + window.scrollY,
+                m_x = e.pageX - rect.left - window.scrollX,
+                m_y = e.pageY - rect.top - window.scrollY;
+
+            let mouseX = m_x + (x);
+            let mouseY = m_y + (y);
+
+            let match = d3.dgenies.mousetip.track_x.get_match(e);
+
+            let html = "";
+            if (match !== null) {
+                html = `<strong>Target:</strong> ${match.contig}<br/>(${match.feature[0]} - ${match.feature[0] + match.feature[1]})<br/>
+                        <strong>Value:</strong> ${match.feature[2]}<br/>
+                        <strong>Comment:</strong> ${match.feature[3]}`
+            }
+            tip.html(html);
+            let css = { top: mouseY, left: mouseX };
+
+            if (!hidden && ((!e.ctrlKey && !d3.dgenies.zone_selected) || match !== null)) {
+                tip.show().css(css);
+            }
+
+        }, 0);
+    });
+};
+
+
+$.fn.mousetip_track_y = function(my_tip, relative_to = null, x = 20, y = 20) {
+    let $this = $(this);
+    console.log($this)
+    let tip = relative_to === null ? $(my_tip, this) : $(my_tip, relative_to);
+    let hidden = true;
+
+    $this.hover(function (e) {
+        if (!e.ctrlKey) {
+            tip.show();
+            hidden = false;
+        }
+
+    }, function () {
+        hidden = true;
+        tip.hide().removeAttr('style');
+
+    }).mousemove(function (e) {
+
+        tip.hide();
+
+        window.setTimeout(() => {
+
+            let rect = relative_to === null ? this.getBoundingClientRect() : $(relative_to)[0].getBoundingClientRect();
+            let posX = rect.left + window.scrollX,
+                posY = rect.top + window.scrollY,
+                m_x = e.pageX - rect.left - window.scrollX,
+                m_y = e.pageY - rect.top - window.scrollY;
+
+            let mouseX = m_x + (x);
+            let mouseY = m_y + (y);
+
+            let match = d3.dgenies.mousetip.track_y.get_match(e);
+
+            let html = "";
+            if (match !== null) {
+                html = `<strong>Query:</strong> ${match.contig}<br/>(${match.feature[0]} - ${match.feature[0] + match.feature[1]})<br/>
+                        <strong>Value:</strong> ${match.feature[2]}<br/>
+                        <strong>Comment:</strong> ${match.feature[3]}`
+            }
+            tip.html(html);
+            let css = { top: mouseY, left: mouseX };
+
+            if (!hidden && ((!e.ctrlKey && !d3.dgenies.zone_selected) || match !== null)) {
+                tip.show().css(css);
+            }
+
+        }, 0);
+    });
+};
+
+
 /**
- * Initialise tooltip
+ * Initialise tooltips
  */
 d3.dgenies.mousetip.init = function () {
-    $("#draw").append($("<span>").attr("class", "tip"));
-    $("g.container").mousetip(".tip", "#draw");
+    d3.dgenies.mousetip.dotplot.init();
+    d3.dgenies.mousetip.track_x.init();
+    d3.dgenies.mousetip.track_y.init();
 };
 
 /**
- * Hide tooltip
+ * Hide tooltips
  */
 d3.dgenies.mousetip.hide = function () {
     $(".tip", "#draw").hide();
-}
+};
 
 /**
  * get label to show
@@ -166,12 +276,21 @@ d3.dgenies.mousetip.get_label = function (label) {
 };
 
 /**
- * Get match override by mouse cursor
+ * Initialise tooltip for dotplot
+ */
+d3.dgenies.mousetip.dotplot.init = function () {
+    $("#draw").append($("<span>").attr("class", "tip"));
+    $("g.container").mousetip_dotplot(".tip", "#draw");
+};
+
+
+/**
+ * Get match override by mouse cursor on dotplot
  *
  * @param e mouse event
  * @returns {{x_zone: string, y_zone: string, x_match: float[], y_match: float[], idy: float}}
  */
-d3.dgenies.mousetip.get_match = function(e) {
+d3.dgenies.mousetip.dotplot.get_match = function(e) {
     let rect = $("g.container")[0].getBoundingClientRect();
     let posX = rect.left + window.scrollX,
         posY = rect.top + window.scrollY,
@@ -233,4 +352,102 @@ d3.dgenies.mousetip.get_match = function(e) {
         }
     }
     return null;
+}
+
+
+/**
+ * Initialise tooltip for dotplot
+ */
+d3.dgenies.mousetip.track_x.init = function () {
+    $("svg.top-track").mousetip_track_x(".tip", "#draw");
+};
+
+/**
+ * Get match override by mouse cursor on track x
+ *
+ * @param e mouse event
+ * @returns {{x_zone: string, y_zone: string, x_match: float[], y_match: float[], idy: float}}
+ */
+d3.dgenies.mousetip.track_x.get_match = function (e) {
+    // Get DOM object coordinates the mouse cursor is over.
+    let rect = $("svg.top-track")[0].getBoundingClientRect();
+    let posX = rect.left + window.scrollX,
+        posY = rect.top + window.scrollY,
+        width_c = rect.width,
+        height_c = rect.height;
+    console.log(posX, posY, width_c, height_c)
+
+    // Transform into coordinates in data
+    let c_x = (e.pageX - posX) / width_c * d3.dgenies.scale
+    c_x = c_x / d3.dgenies.scale * d3.dgenies.x_len;
+
+    let error_x = d3.dgenies.content_lines_width / d3.dgenies.scale * d3.dgenies.x_len;
+    // let error_x = 0,
+
+    let match = null;
+    let found = false;
+    let x_track = d3.dgenies.x_track.data;
+    for (let x_id in x_track) {
+        for (let feat of x_track[x_id]) {
+            let f_start = feat[0];
+            let f_len = feat[1];
+            if (f_start <= c_x && c_x <= f_start + f_len) {
+                match = { contig: x_id, feature: feat };
+                found = true;
+                break;
+            }
+        }
+        if (found) { break; }
+    }
+
+    return match;
+}
+
+
+/**
+ * Initialise tooltip for dotplot
+ */
+d3.dgenies.mousetip.track_y.init = function () {
+    $("svg.right-track").mousetip_track_y(".tip", "#draw");
+};
+
+/**
+ * Get match override by mouse cursor on track y
+ *
+ * @param e mouse event
+ * @returns {{x_zone: string, y_zone: string, x_match: float[], y_match: float[], idy: float}}
+ */
+d3.dgenies.mousetip.track_y.get_match = function (e) {
+    // Get DOM object coordinates the mouse cursor is over.
+    let rect = $("svg.right-track")[0].getBoundingClientRect();
+    let posX = rect.left + window.scrollX,
+        posY = rect.top + window.scrollY,
+        width_c = rect.width,
+        height_c = rect.height;
+    console.log(posX, posY, width_c, height_c)
+
+    // Transform into coordinates in data
+    let c_y = d3.dgenies.scale - ((e.pageY - posY) / height_c * d3.dgenies.scale);
+    c_y = c_y / d3.dgenies.scale * d3.dgenies.y_len;
+
+    let error_y = d3.dgenies.content_lines_width / d3.dgenies.scale * d3.dgenies.y_len;
+    // let error_x = 0,
+
+    let match = null;
+    let found = false;
+    let x_track = d3.dgenies.y_track.data;
+    for (let x_id in x_track) {
+        for (let feat of x_track[x_id]) {
+            let f_start = feat[0];
+            let f_len = feat[1];
+            if (f_start <= c_y && c_y <= f_start + f_len) {
+                match = { contig: x_id, feature: feat };
+                found = true;
+                break;
+            }
+        }
+        if (found) { break; }
+    }
+
+    return match;
 }
